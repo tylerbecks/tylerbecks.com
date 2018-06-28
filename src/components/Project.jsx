27ec6fb6074d0073216535cb15fb10723aefcbc3
Tyler Becks <tylerdbecks@gmail.com>
2018-06-27 19:26:41 -0700

    Offset images

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -2,17 +2,37 @@ import React from 'react'
 import injectSheet from 'react-jss'
 import { Container, Col, Row } from 'reactstrap'
 
-const styles = {}
+const styles = {
+  base: {
+    marginBottom: '2rem',
+  },
+  image: {
+    maxWidth: '100%',
+    boxShadow: `0px 1px 5px 0px rgba(0, 0, 0, 0.2),
+      0px 2px 2px 0px rgba(0, 0, 0, 0.14),
+      0px 3px 1px -2px rgba(0, 0, 0, 0.12)`,
+  },
+  imageRight: {
+    float: 'right',
+  },
+}
 
-const Project = ({ imageSrc, alt, description, title }) => (
-  <Container>
+const Project = ({ classes, imageSrc, alt, description, title, offset }) => (
+  <Container fluid className={classes.base}>
     <Row>
-      <Col>
-        <img src={imageSrc} alt={alt} />
+      <Col md={{ size: 7, order: offset ? 2 : 1 }}>
+        <img
+          className={`${classes.image} ${offset ? classes.imageRight : ''}`}
+          src={imageSrc}
+          alt={alt}
+        />
       </Col>
-      <Col>
+      <Col
+        md={{ size: 5, order: offset ? 1 : 2 }}
+        className="justify-content-center"
+      >
         <h3>{title}</h3>
-        <p>{description}</p>
+        <p className="text-justify">{description}</p>
       </Col>
     </Row>
   </Container>
--- src/pages/landingSections/Projects.jsx
+++ src/pages/landingSections/Projects.jsx
@@ -1,61 +1,112 @@
 import React from 'react'
 import injectSheet from 'react-jss'
 import Project from '../../components/Project'
-import occupancy from '../../images/occupancy.png'
-import alerts from '../../images/alerts.png'
-import oversell from '../../images/oversell.png'
-import reddit from '../../images/reddit.gif'
-import hoarders from '../../images/hoarders.gif'
-import eatup from '../../images/eatup.png'
-import pandorasbox from '../../images/pandorasbox.png'
+import occupancyImg from '../../images/occupancy.png'
+import alertsImg from '../../images/alerts.png'
+import oversellImg from '../../images/oversell.png'
+import redditImg from '../../images/reddit.gif'
+import hoardersImg from '../../images/hoarders.gif'
+import eatupImg from '../../images/eatup.png'
+import pandorasboxImg from '../../images/pandorasbox.png'
+
+const occupancy = {
+  image: occupancyImg,
+  alt: 'Smarking occupancy data chart',
+  description: `The bread and butter of the dashboard.   Here, customers
+    can analyze their occupancy, revenue, and duration data. They can slice
+    and dice the data by time, type of parker, peak vs. average, etc. We
+    used the amCharts library for the visualization and layered many 
+    features on top, such as last year comparison, adding annotations, 
+    downloading the chart and data, and an in-house legend with customizable
+    colors.`,
+  title: 'Data Visualization',
+}
+const alerts = {
+  image: alertsImg,
+  alt: 'Smarking alerts creation modal',
+  description: `Alerts and anomaly detection alert users when occupancy exceeds 
+    a specified threshold. Users can choose to receive alerts via email or text. 
+    We used Mandrill for email alerts and Twilio for text alerts.`,
+  title: 'Alerts',
+}
+const oversell = {
+  image: oversellImg,
+  alt: 'Smkarking oversell tool',
+  description: `This tool helps customers make informed decisions on how to properly 
+    oversell particular groups of their tenants/parkers. Based on each group’s 
+    historical usage patterns, the tool calculates the ideal oversell percentage.`,
+  title: 'Oversell',
+}
+
+const reddit = {
+  image: redditImg,
+  alt: 'reddit dating app signup flow',
+  description: `I built this dating app to match users based on common subreddit 
+    interests. Users sign up with a Reddit account and feed our database with their 
+    stats and trends from Reddit. It was my first time creating a recommendation 
+    engine, which led to my discovery of the graph database, Neo4j. The first 
+    algorithm created links between users that followed the same subreddit. On the 
+    next iteration, I optimized the query to prioritize uncommon subreddits and users 
+    that previously "liked" another user. I built the frontend with React and cached 
+    user data in Redux. To optimize the system architecture for scalability, I broke 
+    out a microservice to store user data in a PostgreSQL database.`,
+  title: 'Reddi2Mingle',
+}
+const hoarders = {
+  image: hoardersImg,
+  alt: 'hoarders game-play demo',
+  description: `Pokemon Go gave me the inspiration to create a game that would encourage 
+    people to get outside. In Hoarders, the goal is to collect coins around the city by 
+    getting within physical range and banking them at your home base. I used the browser's 
+    geolocation API to track and update player coordinates. I also enabled multiplayer 
+    functionality by streaming player locations with Socket.io.`,
+  title: 'Hoarders',
+}
+const eatup = {
+  image: eatupImg,
+  alt: 'eatup dashboard',
+  description: `A great meal comes with great company. EatUp solves the problem of eating 
+    alone. On EatUp, users can create new "EatUp" events by selecting a restaurant from the
+    Google Places API and posting it to the site. When a user wants to join an EatUp, the 
+    results displayed on the dashboard are filtered based on the user's location. I used 
+    React to build the frontend components, Node and Express for the backend, and MySQL to 
+    store all the user data and content.`,
+  title: 'EatUp',
+}
+const pandorasbox = {
+  image: pandorasboxImg,
+  alt: "pandora's box question page",
+  description: `Many stories go untold because they never find the appropriate time and place. 
+    I built Pandora's Box to give people a place to share their interesting stories in a 
+    non-judgmental community. Designed with a mobile-first approach in Angular and the backend 
+    is Node and Express. I used MongoDB to store anonymous user responses.`,
+  title: "Pandora's Box",
+}
+
+const projects = [
+  occupancy,
+  alerts,
+  oversell,
+  reddit,
+  hoarders,
+  eatup,
+  pandorasbox,
+]
 
 const styles = {}
 
 const Projects = ({}) => (
   <section>
-    <Project
-      imageSrc={occupancy}
-      alt="Smarking occupancy data chart"
-      description="The bread and butter of the dashboard. Here, customers can analyze their occupancy, revenue, and duration data. They can slice and dice the data by time, type of parker, peak vs. average, etc. We used the amCharts library for the visualization and layered many features on top, such as last year comparison, adding annotations, downloading the chart and data, and an in-house legend with customizable colors."
-      title="Data Visualization"
-    />
-    <Project
-      imageSrc={alerts}
-      alt="Smarking alerts creation modal"
-      description="Alerts and anomaly detection alert users when occupancy exceeds a specified threshold. Users can choose to receive alerts via email or text. We used Mandrill for email alerts and Twilio for text alerts."
-      title="Alerts"
-    />
-    <Project
-      imageSrc={oversell}
-      alt="Smkarking oversell tool"
-      description="This tool helps customers make informed decisions on how to properly oversell particular groups of their tenants/parkers. Based on each group’s historical usage patterns, the tool calculates the ideal oversell percentage."
-      title="Oversell"
-    />
-
-    <Project
-      imageSrc={reddit}
-      alt="reddit dating app signup flow"
-      description={`I built this dating app to match users based on common subreddit interests. Users sign up with a Reddit account and feed our database with their stats and trends from Reddit. It was my first time creating a recommendation engine, which led to my discovery of the graph database, Neo4j. The first algorithm created links between users that followed the same subreddit. On the next iteration, I optimized the query to prioritize uncommon subreddits and users that previously "liked" another user. I built the frontend with React and cached user data in Redux. To optimize the system architecture for scalability, I broke out a microservice to store user data in a PostgreSQL database.`}
-      title="Reddi2Mingle"
-    />
-    <Project
-      imageSrc={hoarders}
-      alt="hoarders game-play demo"
-      description={`Pokemon Go gave me the inspiration to create a game that would encourage people to get outside. In Hoarders, the goal is to collect coins around the city by getting within physical range and banking them at your home base. I used the browser's geolocation API to track and update player coordinates. I also enabled multiplayer functionality by streaming player locations with Socket.io.`}
-      title="Hoarders"
-    />
-    <Project
-      imageSrc={eatup}
-      alt="eatup dashboard"
-      description={`A great meal comes with great company. EatUp solves the problem of eating alone. On EatUp, users can create new "EatUp" events by selecting a restaurant from the Google Places API and posting it to the site. When a user wants to join an EatUp, the results displayed on the dashboard are filtered based on the user's location. I used React to build the frontend components, Node and Express for the backend, and MySQL to store all the user data and content.`}
-      title="EatUp"
-    />
-    <Project
-      imageSrc={pandorasbox}
-      alt="pandora's box question page"
-      description={`Many stories go untold because they never find the appropriate time and place. I built Pandora's Box to give people a place to share their interesting stories in a non-judgmental community. Designed with a mobile-first approach in Angular and the backend is Node and Express. I used MongoDB to store anonymous user responses.`}
-      title="Pandora's Box"
-    />
+    {projects.map(({ image, alt, description, title }, index) => (
+      <Project
+        key={image}
+        imageSrc={image}
+        alt={alt}
+        description={description}
+        title={title}
+        offset={index % 2 !== 0}
+      />
+    ))}
   </section>
 )
 
