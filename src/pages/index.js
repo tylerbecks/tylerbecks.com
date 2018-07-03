4ea0e13829bb6e85c783235040a477ad77262b5e
Tyler Becks <tylerdbecks@gmail.com>
2018-07-03 15:28:19 -0700

    Add jss-nested

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,10 @@ import React from 'react'
 import AboveTheFold from './landingSections/AboveTheFold'
 import Intro from './landingSections/Intro'
 import Projects from './landingSections/Projects'
+import jss from 'jss'
+import jssNested from 'jss-nested'
+
+jss.setup(jssNested())
 
 const IndexPage = () => (
   <div>
