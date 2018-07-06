8e2465ba79dfdcaa6ef6df0ca8b6e589b9fd2172
Tyler Becks <tylerdbecks@gmail.com>
2018-07-06 00:12:42 -0700

    Add favicon and facebook icon info to Helmet

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -1,6 +1,33 @@
 import React from 'react'
 import { Helmet } from 'react-helmet'
 
-const Head = ({ siteTitle }) => <Helmet title={siteTitle} />
+const Head = ({ siteTitle }) => (
+  <Helmet title={siteTitle}>
+    {/* Favicon */}
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+    <link rel="manifest" href="/site.webmanifest" />
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+    <meta name="msapplication-TileColor" content="#da532c" />
+    <meta name="theme-color" content="#ffffff" />
+    {/* Favicon */}
+
+    {/* Facebook Meta */}
+    <meta property="og:image:height" content="496" />
+    <meta property="og:image:width" content="948" />
+    <meta
+      property="og:description"
+      content="Check out everything Tyler has been working on"
+    />
+    <meta property="og:title" content="Tyler Becks' Portfolio" />
+    <meta property="og:url" content="http://tylerbecks.com/" />
+    <meta
+      property="og:image"
+      content="http://tylerbecks.com/img/facebook/og-image.jpg"
+    />
+    {/* Facebook Meta */}
+  </Helmet>
+)
 
 export default Head
