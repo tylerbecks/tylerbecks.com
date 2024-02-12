efd7dd1ba3ee12cae1b941e0185fe99ca20e41cf
Tyler Becks <tylerdbecks@gmail.com>
2024-02-12 19:22:30 +0800

    Replace everything with Astro starter

--- gatsby-node.js
+++
@@ -1,7 +0,0 @@
-const Shell = require('child_process')
-
-const onPostBuild = () => {
-  Shell.execSync('cp -r assets/* public/')
-  Shell.execSync('cp -r assets/favicon/* public/')
-}
-module.exports = { onPostBuild }
--- src/utils/styles.js
+++
@@ -1,3 +0,0 @@
-export const white = '#fff'
-export const offWhite = '#f8f8f8'
-export const darkGray = '#26262a'
