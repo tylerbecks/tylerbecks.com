7a23e344e616bb93a5f85e2a3c2d51f1b3470cfd
Tyler Becks <tylerdbecks@gmail.com>
2024-11-20 13:43:07 -0800

    Add eslint and prettier

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,14 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import netlify from '@astrojs/netlify';
-import react from "@astrojs/react";
+import react from '@astrojs/react';
 
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://tylerbecks.com',
   integrations: [mdx(), sitemap(), react(), tailwind()],
   output: 'server',
-  adapter: netlify()
+  adapter: netlify(),
 });
